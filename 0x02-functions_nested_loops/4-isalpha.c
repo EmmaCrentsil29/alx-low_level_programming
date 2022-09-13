0998b59011a400eb8c35ce412ae8b99f0fe5bfca
EmmaCrentsil29 <emillycaesalpina@gmail.com>
2022-09-13 16:36:12 -0700

    work

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,9 @@
 /**
  * print_sign - function to check for a sign of a number
  * @c: is the int that will use for the argument of the function
- * Return: 0
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
