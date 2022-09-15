40c9725e72c958967b2e248710e9d1a2ff4dbfb2
EmmaCrentsil29 <emillycaesalpina@gmail.com>
2022-09-15 05:54:44 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,4 +15,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
+#endif /* MAIN_H */
