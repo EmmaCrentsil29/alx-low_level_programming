aa64a74b9756ee415d5e39111d5066cd9ba6da29
EmmaCrentsil29 <emillycaesalpina@gmail.com>
2022-10-06 11:50:07 -0700

    correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -121,8 +121,8 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	{
 		if (*mult < '0' || *mult > '9')
 		{
-			printf("Error\n")
-				exit(98);
+			printf("Error\n");
+			exit(98);
 		}
 
 		num = (*mult - '0') * digit;
