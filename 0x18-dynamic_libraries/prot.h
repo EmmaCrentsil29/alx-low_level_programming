68270119420a2e6c65db28af141f481ce9086640
EmmaCrentsil29 <emillycaesalpina@gmail.com>
2022-12-13 10:37:51 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/prot.h
@@ -0,0 +1,11 @@
+#ifndef PROT_H
+#define PROT_H
+#include <stdio.h>
+
+int div(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+int sub(int x, int y);
+int mod(int x, int y);
+
+#endif
